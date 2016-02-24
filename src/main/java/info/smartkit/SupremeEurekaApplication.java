eeb627fa12821bb813e05e95c9da918e43b71a48
yangboz <zhou.yangbo@intellif.com>
2016-02-24 12:07:21 +0800

    ADDED:spring-boot starter barebone code base related files

---
+++ src/main/java/info/smartkit/SupremeEurekaApplication.java
@@ -0,0 +1,12 @@
+package info.smartkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremeEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupremeEurekaApplication.class, args);
+	}
+}
